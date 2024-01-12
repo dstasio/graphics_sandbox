f55341a57f82fa47a3ed42218cdd83609bc884e2
Davide Stasio <doctegl@gmail.com>
2024-01-12 21:07:34 +0100

    Version 1

--- code/gs/gs.h
+++ code/gs/gs.h
@@ -1,8 +1,23 @@
-//#include "datatypes.h"
+// Version 1
 
-// @note: On windows, requires linking against 'Gdi32.lib'
+//
+// Config variables
+//
+#define GS_INTERNAL 1
+
+#define GS_DRAG_WITH_MIDDLE_CLICK 1
+#define GS_DRAG_WITH_RIGHT_CLICK  0
+
+#define GS_WINDOW_WIDTH  1500
+#define GS_WINDOW_HEIGHT  950
+
+//
+// Platform flags
+//
 #if defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__)
     #define GS_WIN   1
+    #pragma comment(lib, "gdi32.lib")
+    #pragma comment(lib, "user32.lib")
 #elif defined(__APPLE__) || defined(__MACH__)
     #define GS_MACOS 1
 #elif defined(unix) || defined(__unix) || defined(__unix__)
@@ -35,16 +50,38 @@ struct gs_v2
     float x;
     float y;
 };
-gs_v2  operator -  (gs_v2 &a)           { return {-a.x, -a.y}; }
-gs_v2  operator *  (gs_v2  a, float  b) { return { a.x * b, a.y * b}; }
-gs_v2  operator +  (gs_v2  a, gs_v2  b) { return { a.x + b.x, a.y + b.y }; }
-gs_v2  operator -  (gs_v2  a, gs_v2  b) { return { a.x - b.x, a.y - b.y }; }
-gs_v2 &operator *= (gs_v2 &a, float  b) { a.x *= b  ; a.y *= b  ; return a; }
-gs_v2 &operator *= (gs_v2 &a, gs_v2 &b) { a.x *= b.x; a.y *= b.y; return a; }
-gs_v2 &operator += (gs_v2 &a, gs_v2 &b) { a.x += b.x; a.y += b.y; return a; }
-gs_v2 &operator -= (gs_v2 &a, gs_v2 &b) { a.x -= b.x; a.y -= b.y; return a; }
+gs_v2  operator -  (gs_v2  a)          { return {-a.x, -a.y}; }
+gs_v2  operator *  (gs_v2  a, float b) { return { a.x * b, a.y * b}; }
+gs_v2  operator +  (gs_v2  a, gs_v2 b) { return { a.x + b.x, a.y + b.y }; }
+gs_v2  operator -  (gs_v2  a, gs_v2 b) { return { a.x - b.x, a.y - b.y }; }
+gs_v2 &operator *= (gs_v2 &a, float b) { a.x *= b  ; a.y *= b  ; return a; }
+gs_v2 &operator *= (gs_v2 &a, gs_v2 b) { a.x *= b.x; a.y *= b.y; return a; }
+gs_v2 &operator /= (gs_v2 &a, float b) { a.x /= b  ; a.y /= b  ; return a; }
+gs_v2 &operator /= (gs_v2 &a, gs_v2 b) { a.x /= b.x; a.y /= b.y; return a; }
+gs_v2 &operator += (gs_v2 &a, gs_v2 b) { a.x += b.x; a.y += b.y; return a; }
+gs_v2 &operator -= (gs_v2 &a, gs_v2 b) { a.x -= b.x; a.y -= b.y; return a; }
 
-gs_v2 gs_make_v2(float x, float y) { return {x, y}; }
+struct gs_v3
+{
+    float x;
+    float y;
+    float z;
+};
+gs_v3  operator -  (gs_v3  a)          { return {-a.x, -a.y, -a.z}; }
+gs_v3  operator +  (gs_v3  a, gs_v3 b) { return { a.x + b.x, a.y + b.y, a.z + b.z }; }
+gs_v3  operator -  (gs_v3  a, gs_v3 b) { return { a.x - b.x, a.y - b.y, a.z - b.z }; }
+gs_v3 &operator /= (gs_v3 &a, float b) { a.x /= b  ; a.y /= b  ; a.z /= b  ; return a; }
+gs_v3 &operator /= (gs_v3 &a, gs_v3 b) { a.x /= b.x; a.y /= b.y; a.z /= b.z; return a; }
+gs_v3 &operator += (gs_v3 &a, gs_v3 b) { a.x += b.x; a.y += b.y; a.z += b.z; return a; }
+gs_v3 &operator -= (gs_v3 &a, gs_v3 b) { a.x -= b.x; a.y -= b.y; a.z -= b.z; return a; }
+
+gs_v3 gs_cross(gs_v3 a, gs_v3 b) { return {a.y*b.z - a.z*b.y,
+                                           a.z*b.x - a.x*b.z,
+                                           a.x*b.y - a.y*b.x}; }
+
+gs_v2 gs_make_v2(float x, float y)          { return {  x,   y   }; }
+gs_v3 gs_make_v3(float x, float y, float z) { return {  x,   y, z}; }
+gs_v3 gs_make_v3(         gs_v2 v, float z) { return {v.x, v.y, z}; }
 
 // ==========================================================================
 //
@@ -132,7 +169,7 @@ struct GS_State
 static GS_State * gs_state;
 static GS_State  _gs_default_state;
 
-// this works for windows
+// this works for windows, might need to be changed on other platforms
 #define GS_RGB(r, g, b) (((r) << 16) | \
                          ((g) <<  8) | \
                          ((b)      ))
@@ -144,7 +181,7 @@ static GS_State  _gs_default_state;
 #define GS_BLUE   GS_RGB(0x00, 0x87, 0xDF)
 
 #define GS_YELLOW  GS_RGB(0xFF, 0xD3, 0x00)
-#define GS_MAGENTA
+#define GS_MAGENTA GS_RGB(0xF4, 0x66, 0xF4)
 #define GS_CYAN    GS_RGB(0x00, 0xEF, 0xDF)
 
 bool gs_window_2d();
@@ -154,7 +191,10 @@ void gs_draw_point(float x, float y, uint32_t color = GS_GREY(0xFF), float point
 
 void gs_draw_line          (gs_v2 start_point, gs_v2 end_point, uint32_t color);
 void gs_draw_line_on_screen(int32_t x0, int32_t y0, uint32_t c0, int32_t x1, int32_t y1, uint32_t c1);
+void gs_draw_quad          (float   minx, float   miny, float   maxx, float   maxy, uint32_t color);
 void gs_draw_quad_on_screen(int32_t minx, int32_t miny, int32_t maxx, int32_t maxy, uint32_t color);
+void gs_draw_quad_on_screen(gs_v2 p0, gs_v2 p1, gs_v2 p2, gs_v2 p3, uint32_t color);
+void gs_draw_triangle_on_screen(gs_v2 p0, gs_v2 p1, gs_v2 p2, uint32_t color);
 
 void gs_swap_buffers();
 void gs_clear(uint32_t color = GS_GREY(0));
@@ -172,6 +212,13 @@ int32_t _gs_abs(int32_t a)
     return a;
 }
 
+inline
+float _gs_sign(float a)
+{
+    if (a < 0) return -1;
+    return 1;
+}
+
 
 // =========================================================================
 // Settings
@@ -181,7 +228,7 @@ int32_t _gs_abs(int32_t a)
 // =========================================================================
 
 #define _gs_array_len(arr)   (sizeof(arr) / sizeof(arr[0]))
-#define _gs_swap(a, b, type) { type _temp = (a); a = (b); b = _temp; }
+#define _gs_swap(a, b, type) { type _temp = (a); (a) = (b); (b) = _temp; }
 
 #if GS_INTERNAL
 #include <stdio.h>
@@ -244,9 +291,6 @@ _gs_win32_window_proc(HWND window, UINT message, WPARAM w, LPARAM l)
 
     return result;
 }
-
-#define GS_WINDOW_WIDTH  1024
-#define GS_WINDOW_HEIGHT  720
 
 bool gs_window_2d()
 {
@@ -320,11 +364,16 @@ bool gs_window_2d()
         {
             case WM_MBUTTONDOWN: {
                 gs_state->current_input.mouse_middle = GS_PRESSED;
-            } // passthrough
+#if GS_DRAG_WITH_MIDDLE_CLICK
+                gs_state->is_dragging_origin = true;
+#endif
+            } break;
             case WM_RBUTTONDOWN:
             {
                 gs_state->current_input.mouse_right = GS_PRESSED;
+#if GS_DRAG_WITH_RIGHT_CLICK
                 gs_state->is_dragging_origin = true;
+#endif
             } break;
 
             case WM_LBUTTONDOWN: {
@@ -336,17 +385,18 @@ bool gs_window_2d()
 
             case WM_MBUTTONUP: {
                 gs_state->current_input.mouse_middle = GS_JUST_RELEASED;
-            } // passthrough
+#if GS_DRAG_WITH_MIDDLE_CLICK
+                gs_state->is_dragging_origin = false;
+#endif
+            } break;
             case WM_RBUTTONUP: {
                 gs_state->current_input.mouse_right = GS_JUST_RELEASED;
+#if GS_DRAG_WITH_RIGHT_CLICK
                 gs_state->is_dragging_origin = false;
+#endif
             } break;
             case WM_MOUSEWHEEL: {
-#if 0
-                gs_v2 mouse_8os = gs_state->current_input.mouse_pos;
-                //mouse_pos -= gs_state->origin;
-                gs_v2 prev_origin = -mouse_pos * (1.f / gs_state->view_scale);
-#endif
+                gs_v2 old_mouse_world = gs_screen_to_world(gs_state->current_input.mouse_pos);
 
                 int wheel_delta = GET_WHEEL_DELTA_WPARAM(message.wParam);
                 if (!wheel_delta) break;
@@ -354,9 +404,9 @@ bool gs_window_2d()
                 if (gs_state->view_scale < GS_MIN_VIEW_SCALE)
                     gs_state->view_scale = GS_MIN_VIEW_SCALE;
 
-#if 0
-                gs_state->origin = gs_state->current_input.mouse_pos + prev_origin * gs_state->view_scale;
-#endif
+                gs_v2 new_mouse_world = gs_screen_to_world(gs_state->current_input.mouse_pos);
+
+                gs_state->origin += new_mouse_world - old_mouse_world;
             } break;
 
             case WM_MOUSEMOVE: {
@@ -568,6 +618,22 @@ void gs_draw_pixel(int32_t x, int32_t y, uint32_t color)
     ((uint32_t *)gs_state->backbuffer)[y * gs_state->backbuffer_width + x] = color;
 }
 
+void gs_draw_quad(float minx, float miny, float maxx, float maxy, uint32_t color)
+{
+    _gs_assert(minx <= maxx);
+    _gs_assert(miny <= maxy);
+
+    _gs_assert(!isnan(minx));
+    _gs_assert(!isnan(miny));
+    _gs_assert(!isnan(maxx));
+    _gs_assert(!isnan(maxy));
+    gs_v2 screen_min = gs_world_to_screen({minx, miny});
+    gs_v2 screen_max = gs_world_to_screen({maxx, maxy});
+
+    gs_draw_quad_on_screen((int32_t)screen_min.x, (int32_t)screen_min.y,
+                           (int32_t)screen_max.x, (int32_t)screen_max.y, color);
+}
+
 void gs_draw_quad_on_screen(int32_t minx, int32_t miny, int32_t maxx, int32_t maxy, uint32_t color)
 {
     _gs_assert(minx <= maxx);
@@ -582,6 +648,11 @@ void gs_draw_quad_on_screen(int32_t minx, int32_t miny, int32_t maxx, int32_t ma
 
         ((uint32_t *)gs_state->backbuffer)[yy * gs_state->backbuffer_width + xx] = color;
     }}
+}
+
+void gs_draw_quad_on_screen(gs_v2 p0, gs_v2 p1, gs_v2 p2, gs_v2 p3, uint32_t color) {
+    gs_draw_triangle_on_screen(p0, p1, p2, color);
+    gs_draw_triangle_on_screen(p0, p2, p3, color);
 }
 
 void gs_clear(uint32_t color)
@@ -660,7 +731,7 @@ void gs_draw_line_on_screen(int32_t x0, int32_t y0, uint32_t c0,
 
     //float color_t = 0.f;
     //float color_tStep = 1.f / (float)(dx);
-    for (x = x0; x <= x1; x++)  {
+    for (x = x0; x <= x1; x += 1)  {
         //setPixel(*pixelX, *pixelY, lerp(c0, c1, color_t));
         //color_t += color_tStep;
         gs_draw_pixel(*pixel_x, *pixel_y, c0);
@@ -672,5 +743,99 @@ void gs_draw_line_on_screen(int32_t x0, int32_t y0, uint32_t c0,
         }
     }
 }
+
+void gs_draw_triangle_on_screen(gs_v2 p0, gs_v2 p1, gs_v2 p2, uint32_t color) {
+    // slow, accurate version
+    gs_v3 min = {p0.x, p0.y, 0};
+    gs_v3 max = {p0.x, p0.y, 0};
+
+    if (p1.x < min.x)  min.x = p1.x;
+    if (p2.x < min.x)  min.x = p2.x;
+    if (p1.y < min.y)  min.y = p1.y;
+    if (p2.y < min.y)  min.y = p2.y;
+    if (p1.x > max.x)  max.x = p1.x;
+    if (p2.x > max.x)  max.x = p2.x;
+    if (p1.y > max.y)  max.y = p1.y;
+    if (p2.y > max.y)  max.y = p2.y;
+
+    //gs_draw_quad_on_screen((int32_t)min.x, (int32_t)min.y,
+                           //(int32_t)max.x, (int32_t)max.y,
+                           //GS_GREY(200));
+
+    int32_t min_x = (int32_t)min.x;
+    int32_t min_y = (int32_t)min.y;
+    int32_t max_x = (int32_t)max.x;
+    int32_t max_y = (int32_t)max.y;
+
+    gs_v3 v01 = gs_make_v3(p1 - p0, 0);
+    gs_v3 v02 = gs_make_v3(p2 - p0, 0);
+    gs_v3 v12 = gs_make_v3(p2 - p1, 0);
+    for (int y = min_y; y <= max_y; y += 1) {
+    for (int x = min_x; x <= max_x; x += 1) {
+        gs_v3 v = {(float)x, (float)y, 0};
+
+        gs_v3 p0_to_v = v - gs_make_v3(p0, 0);
+        gs_v3 p1_to_v = v - gs_make_v3(p1, 0);
+        float s01 = _gs_sign(gs_cross(v01    , p0_to_v).z);
+        float s02 = _gs_sign(gs_cross(p0_to_v,     v02).z);
+        float s12 = _gs_sign(gs_cross(v12    , p1_to_v).z);
+
+        if (s01 != s02)  continue;
+        if (s01 != s12)  continue;
+
+        gs_draw_pixel(x, y, color);
+    }}
+}
+
+#if 0
+void _gs_draw_triangle_on_screen(gs_v2 p0, gs_v2 p1, gs_v2 p2, uint32_t color) {
+    int32_t x, y;
+    int32_t *pixel_x = &x;
+    int32_t *pixel_y = &y;
+
+    // @todo: sub-pixel precision
+    // @todo: maybe round instead of truncating?
+    int32_t dx = (int32_t)(p1.x - p0.x),
+            dy = (int32_t)(p1.y - p0.y);
+    if (_gs_abs(dx) < _gs_abs(dy))
+    {
+	// @todo: do colors also need to be swapped?
+	_gs_swap(p0.x, p0.y, float);
+	_gs_swap(p1.x, p1.y, float);
+	_gs_swap(dx, dy, uint32_t);
+	pixel_x = &y;
+	pixel_y = &x;
+    }
+
+    if (p1.x < p0.x) {
+	_gs_swap(p0, p1, gs_v2);
+	dx = -dx;
+	dy = -dy;
+    }
+
+    int32_t e = 0,
+	    i = 1;
+    y = (int32_t)p0.y;
+
+    if (p1.y < p0.y)  i = -1;
+
+    //r32 total_dist = distance(p0, p1);
+    for (x = (int32_t)p0.x; x <= (int32_t)p1.x; x += 1)  {
+        // @todo: do this to lerp colors.
+	//gs_v2 curr_p = {(float)x, (float)y};
+	//float      t = distance(p0, curr_p) / total_dist;
+	//gs_v2     uv =    lerp(p0.uv,uv, t);
+
+	gs_draw_line_on_screen(     *pixel_x,      *pixel_y, color,
+                               (int32_t)p2.x, (int32_t)p2.y, color);
+
+	e += dy;
+	if ( ((e << 1)*i) >= dx) {
+	    y +=  1*i;
+	    e -= dx*i;
+	}
+    }
+}
+#endif
 
 #endif // GS_WIN
